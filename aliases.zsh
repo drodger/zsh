e5200b1c69bb14923a91ab769aaf59ea5b3d9c24
Derek Rodger <derek.rodger@gmail.com>
2016-03-08 10:21:47 -0800

    Add scl aliases

--- aliases.zsh
+++ aliases.zsh
@@ -10,3 +10,10 @@ alias an='cd ~/code/ansible'
 alias ad='cd /var/www/erezadmin'
 alias ez='cd /var/www/erezlife'
 alias dev='cd /var/www/erezlife/development'
+
+alias to='ssh toronto.erezlife.com'
+alias fl='ssh florida.erezlife.com'
+
+alias en_py3='source /opt/rh/rh-python34/enable'
+alias en_php='source /opt/rh/rh-php56/enable'
+alias en_pg='source /opt/rh/rh-postgresql94/enable'
